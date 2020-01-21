10b4cc1f9cba54d1c3d8e04f1659c737a45c38f6
Cassquatch <cassidy.fortner.com>
2020-01-21 15:30:41 -0600

    added screenshots for readme and cleaned up spaces and logs in code

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Note-Taker
+Full stack application that allows the user the create and save notes, view saved notes, as well as delete saved notes.
+
+# Screenshots
+Home Page
+![home](./public/assets/screenshots/home.png)
+
+Adding Notes
+![notes](./public/assets/screenshots/notes.png)
+
+Saved Notes
+![saved](./public/assets/screenshots/saved-notes.png)
+
+# Built With
+* HTML
+* CSS
+* Javascript
+* jQuery
+* Node.js
+* Express.js
+
+# Functionality
+The user can click the pen on the right side of the page to create a new note, which has a title and content. Once content has been added to the note, the user may save the note. All saved notes are displayed on the left side of the page, and if clicked on will pull up the note in the main section of the page for viewing. If the red trash can next to the saved notes is clicked, that note will be deleted and removed from the list.
+
+# Authors
+Cassidy Fortner
--- server.js
+++ server.js
@@ -13,10 +13,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//point to route files for handling
-// require("./public/assets/routes/api-routes")(app);
-// require("./public/assets/routes/html-routes")(app);
-
 //add routes for js file / css file
 app.get("/public/assets/css/styles.css", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/assets/css/styles.css"));
@@ -40,58 +36,41 @@ app.get("/", (req, res) => {
 //handle GET routes for the api
 app.get("/api/notes", (req, res) => {
     //read the db.json file to get the saved notes and display them back
-
     fs.readFile("./db/db.JSON", (err, data) => {
         //throw error if present
         if (err) throw err;
         //parse data
-        
-        
-        
         res.json(JSON.parse(data));
-
     });
 });
 
-
-
 //handle POST requests to actually save data into the db.JSON file
 app.post("/api/notes", (req, res) => {
-    
+    //set up note_array and instantiate a newNote object with request body
     let note_array = [];
     let newNote = req.body;
-    
     //read what is currently in db.json and store it in an array
     fs.readFile("./db/db.json", (err, data) => {
         if (err) throw err;
-        
-        console.log(JSON.parse(data));
         //stringify data to put it in the array
         note_array = JSON.parse(data);
-        
-        //not working correctly, figure out a way to add ids to the notes so that when you click on them they show up
-        
-         if(note_array.length === 0){
-             let id = 0;
-             newNote.id = id + 1;
-         }
-         if(note_array.length > 0){
-            let curr_length = note_array.length;
 
-             newNote.id = note_array[curr_length-1].id + 1;
-         }
-        
-        
+        //not working correctly, figure out a way to add ids to the notes so that when you click on them they show up
+        if (note_array.length === 0) {
+            let id = 0;
+            newNote.id = id + 1;
+        }
+        if (note_array.length > 0) {
+            let curr_length = note_array.length;
+            newNote.id = note_array[curr_length - 1].id + 1;
+        }
+
         note_array.push(newNote);
         fs.writeFile("./db/db.json", JSON.stringify(note_array), (err) => {
             if (err) throw err;
-            
         });
     });
-    console.log(newNote);
     res.json(newNote);
-    //write new file with the array
-   
 });
 
 //handle delete
@@ -103,20 +82,17 @@ app.delete("/api/notes/:id", (req, res) => {
 
         let notes_array = JSON.parse(data);
 
-        for(let i = 0; i < notes_array.length; i++){
-            if(note_id === notes_array[i].id){
+        for (let i = 0; i < notes_array.length; i++) {
+            if (note_id === notes_array[i].id) {
                 res.json(notes_array.splice(i, 1));
-                
             }
         }
 
         fs.writeFile("./db/db.json", JSON.stringify(notes_array), (err) => {
             if (err) throw err;
         });
-        
     });
 
-    
 });
 //start listening on either the host port or default port that was set
 app.listen(PORT, () => {
